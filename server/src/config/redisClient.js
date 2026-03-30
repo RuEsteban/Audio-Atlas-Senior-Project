a555a0f48945706ec5738da30cf64b7447baff25
Hunter Stewart <139908741+hstewart438@users.noreply.github.com>
2026-03-30 11:05:15 -0400

    Added in redis cache and cache clearing functions. Updated cache delete calls.

---
+++ server/src/config/redisClient.js
@@ -0,0 +1,10 @@
+// server/src/config/redisClient.js
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN
+});
+
+
+export default redis;
